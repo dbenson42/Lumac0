3a01c4ea62edb2f8bc2f267481975ec46c8e8520
dbenson42 <109686180+dbenson42@users.noreply.github.com>
2022-07-22 09:41:27 +0200

    Add files via upload

---
+++ src/lumac0.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <map>
+#include <math.h>
+#include <thread>
+
+#include "defs.hpp"
+#include "ttables.hpp"
+#include "chess.hpp"
+#include "evaluate.hpp"
+#include "ordering.hpp"
+#include "search.hpp"
+#include "uci.hpp"
+
+using namespace std;
+
+int main() {
+	Chess::Init();
+	TTables::Init();
+	Eval::Init();
+	UCI::Start();
+}
+
